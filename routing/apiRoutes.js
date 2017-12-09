952352cd2ce8dbadff3fcfdadf2afca2f0e048ec
Mimi Chang <mimichang94@yahoo.com>
2017-12-09 16:36:45 +0000

    some routing logic

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,11 +1,24 @@
+var friends = require("../app/data/friends.js");
+
+// Get all info on everyone from the database
 //GET route /api/friends- display a JSON of all possible friends.
+app.get("/api/friends", function(request, response) {
+    response.json(friends);
+});
+
 //POST route /api/friends- handle incoming survey results, handle the compatibility logic
 //POST creates a friend object - name, photo, scores .. imports friends.js from the data folder
-//when u create a new friend object, u push the friend object into array in friends.js
 
-var friends = require ("../app/data/friends.js");
+app.post("/api/friends", function(request, response) {
 
-//friends.push(objectcreated)
+    var newperson = request.body;
+    newperson.routeName = newperson.name.replace(/\s+/g, "").toLowerCase();
+    //when u create a new friend object, u push the friend object into array in friends.js
+    console.log(newperson);
+    //friends.push(objectcreated)
+    friends.push(newperson);
+
+    response.json(newperson);
+});
 
 //GET ROUTE to API/friends link , JSON data displayed when user clicks on the link on bottom of home page
-
--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -1,4 +1,12 @@
+//this file directs the flow of traffic
+
 //GET route to /survey - goes to survey page
 //default goes to home.html
 
-//directs the flow of traffic
+app.get("/home", function(request, response) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/survey", function(request, response) {
+    res.sendFile(path.join(__dirname, "survey.html"));
+});
