9d3b0e0c39370e835980df18cde95a34b6b8991d
Mimi Chang <mimichang94@yahoo.com>
2017-12-09 16:03:21 +0000

    user interface added

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,18 @@
-//need to export friends.js (module.exports = 'variable')
+//this is the "database"-- empty array and dummy data 
+var friends = [{
+        name: "John",
+        photo: "url link",
+        score: [1, 2, 4, 5, 2, 1, 5, 2, 4, 3]
+    },
 
-var friends = [
-    {name: "John",
-    photo: "url link",
-    score: [1, 2, 4, 5, 2, 1, 5, 2, 4, 3]},
-    
-    {name: "Kristen",
-    photo: "url link",
-    score: [2, 3, 5, 1, 4, 3, 5, 1, 5, 2]}
-    
-    ];
+    {
+        name: "Kristen",
+        photo: "url link",
+        score: [2, 3, 5, 1, 4, 3, 5, 1, 5, 2]
+    }
+
+];
 
 
-
-module.exports = friends; 
-
-//empty array or dummy data - this is the "database"
+//export friends.js (module.exports = 'variable')
+module.exports = friends;
